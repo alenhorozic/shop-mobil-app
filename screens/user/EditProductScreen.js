e2675af6b43eb4740a9263aec2beb8337f966e4c
alenhorozic <alenhorozicrhotmail.com>
2021-06-18 21:47:28 +0200

    add Edit new product form

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,14 +1,92 @@
-import React from "react";
-import { Text, View, TextInput, ScrollView } from "react-native";
+import React, {useState,useEffect,useCallback} from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  StyleSheet,
+  Button,
+} from "react-native";
+import Colors from "../../constans/Colors";
+import {useSelector} from 'react-redux';
 
 const EditProductScreen = (props) => {
-    return (
-        <ScrollView>
-        <View>
-            <Text>Title Edit Product Screen</Text>
+    const prodId = props.route.params.productId;
+    const editedProduct = useSelector(state =>state.products.userProducts.find(prod=>prod.id === prodId));
+
+    const [title, setTitle] = useState(editedProduct.title);
+    const [imageUrl, setImageUrl] = useState(editedProduct.imageUrl);
+    const [price, setPrice] = useState(editedProduct.price);
+    const [description, setDescription] = useState(editedProduct.description);
+
+    const submitHandler = useCallback(() => {
+      console.log('Submitting the form !!!!!!!!')
+    }, []);
+
+    useEffect(() => {
+      props.navigation.setParams({submit:submitHandler});
+    }, [submitHandler]);
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
         </View>
-        </ScrollView>
-    )
-}
+        <View style={styles.formControl}>
+          <Text style={styles.label}>ImageURL</Text>
+          <TextInput style={styles.input} value={imageUrl} onChangeText={text => setImageUrl(text)}/>
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input} value={price.toString()} onChangeText={number => setPrice(number)} keyboardType="numeric"/>
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input} value={description} onChangeText={text => setDescription(text)}/>
+        </View>
+        <View style={styles.actions}>
+          <Button
+            color={Colors.primary}
+            title="Save"
+            onPress={submitHandler}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 20,
+  },
+  formControl: {
+    width: "100%",
+  },
+  label: {
+    fontWeight: "bold",
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    backgroundColor: '#99ccff',
+    fontSize:18,
+    borderRadius:7,
+    margin:5,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  actions: {
+    alignItems: "center",
+    marginTop:15
+  },
+});
 
 export default EditProductScreen;
